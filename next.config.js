237d4f4254fbd573caa644ba55d1913b317cb313
sashi <satoshi3104.nishi@gmail.com>
2025-06-12 12:47:17 +0900

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/sashi0034.github.io',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
