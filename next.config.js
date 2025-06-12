3c72069015147b714cb26aaeaef7b48ec07eddf1
sashi <satoshi3104.nishi@gmail.com>
2025-06-12 13:37:51 +0900

    Fix deployment paths and remove unused index

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
-const basePath = isProd ? '/sashi0034.github.io' : ''
+const basePath = ''
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: isProd ? 'export' : undefined,
   basePath,
+  assetPrefix: basePath,
   ...(isProd && { distDir: 'out' }),
   images: {
     unoptimized: true,
