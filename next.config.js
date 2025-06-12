c2a2ad914b3ea14bfb06c1b67dd57f878597c1eb
sashi <satoshi3104.nishi@gmail.com>
2025-06-12 12:56:35 +0900

    設定修正

--- next.config.js
+++ next.config.js
@@ -6,8 +6,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/sashi0034.github.io',
-  trailingSlash: true,
+  // GitHub Pages用の設定は本番環境でのみ適用
+  ...(process.env.NODE_ENV === 'production' && {
+    basePath: '/sashi0034.github.io',
+    trailingSlash: true,
+  }),
 }
 
 module.exports = nextConfig 
